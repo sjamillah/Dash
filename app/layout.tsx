a7a9b46e09271b52e179567670a9710c74f8b9ee
sjamillah <j.ssozi@alustudent.com>
2026-01-08 05:42:53 +0200

    feat: add main app layout with global styles and root page

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Data Nest",
+  description: "Application for the admin dashboard ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar />
+        <div className="flex">
+          <div className="hidden md:block h-[100vh] w-[300px]">
+            <Sidebar />
+          </div>
+          <div className="p-5 w-full md:max-w-[1140px]">
+            {children}
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
